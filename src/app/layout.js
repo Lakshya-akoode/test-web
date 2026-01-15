4205f8ceaa65e37a14ea33f26bb39cb8960d2922
unknown <lakshya@akoode.in>
2026-01-15 19:15:36 +0530

    update on google analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import LayoutWrapper from "@/components/LayoutWrapper";
 import ErrorBoundary from "@/components/ErrorBoundary";
@@ -25,6 +26,20 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-MLFT0396EB"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-MLFT0396EB');
+          `}
+        </Script>
+
         <ErrorBoundary>
           <ToastProvider>
             <LayoutWrapper>{children}</LayoutWrapper>
